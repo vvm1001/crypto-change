ee191b7849ea06b695d9bd8c244fd3ca99cb9203
OleksandrMorozJito <oleksandr.moroz@jito.dev>
2022-12-10 16:02:20 +0200

    [update] Finish wait page

--- assets/js/transaction.js
+++ assets/js/transaction.js
@@ -11,55 +11,99 @@ $(document).ready(function () {
 
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  
+
   $.ajax({
     url:  URL + "/getOrder?id=" + urlParams.get('id'),
     method: 'GET',
     dataType: "json",
     success: function (data) {
-      setInterval(function () {
-        // location.reload();
-        console.log(data)
-      }, 5000);
+      let htmlToRender
+      if (data.status === 'CHECK_PAYED'){
+        htmlToRender = `
+        <section class="transaction">
+        <div class="container transaction__container wow animate__fadeInUp">
+            <h1 class="transaction__title">
+                Обмен <br>
+                ${data.exchange}                    </h1>
+            <div class="transaction__order">
+                Номер заявки <span>${data.id}</span>
+            </div>
+                 <div class="transaction-2">
+                    <div class="transaction__status transaction__status_blue">
+                        ЖДЕМ ПОДТВЕРЖДЕНИЙ
+                    </div>
+                    <div class="transaction__status-text">
+                        Ваша заявка в обработке!
+                    </div>
+                </div>
+                                <div class="transaction__btns">
+                <a class="transaction__btn" href="./index.html">
+                    <img src="./assets/img/transaction/home.svg" alt="home">
+                    Вернуться
+                </a>
+                                    </div>
+        </div>
+        </section>
+        `
+        setInterval(function () {
+          location.reload();
+        }, 5000);
+      } else if(data.status === 'APPROVED') {
+        htmlToRender = `
+        <section class="transaction">
+        <div class="container transaction__container wow animate__fadeInUp">
+            <h1 class="transaction__title">
+                Обмен <br>
+                ${data.exchange}                    </h1>
+            <div class="transaction__order">
+                Номер заявки <span>${data.id}</span>
+            </div>
+                 <div class="transaction-4">
+                    <div class="transaction__status transaction__status_blue">
+                        Ваша заявка успешно выполнена
+                    </div>
+                    <div class="transaction__status-text">
+                        Спасибо что выбираете нас!
+                    </div>
+                </div>
+                                <div class="transaction__btns">
+                <a class="transaction__btn" href="./index.html">
+                    <img src="./assets/img/transaction/home.svg" alt="home">
+                    Вернуться
+                </a>
+                                    </div>
+        </div>
+        </section>
+        `
+      } else {
+        htmlToRender = `
+        <section class="transaction">
+        <div class="container transaction__container wow animate__fadeInUp">
+            <h1 class="transaction__title">
+                Обмен <br>
+                ${data.exchange}                    </h1>
+            <div class="transaction__order">
+                Номер заявки <span>${data.id}</span>
+            </div>
+                 <div class="transaction-4">
+                    <div class="transaction__status transaction__status_red">
+                      Заявка отклонена
+                    </div>
+                    <div class="transaction__status-text">
+                      К сожалению ваша заявка отклонена
+                    </div>
+                </div>
+                                <div class="transaction__btns">
+                <a class="transaction__btn" href="./index.html">
+                    <img src="./assets/img/transaction/home.svg" alt="home">
+                    Вернуться
+                </a>
+                                    </div>
+        </div>
+        </section>
+        `
+      }
+      document.querySelector('.main').innerHTML = htmlToRender;
     }
   });
-
-  $(".transaction__block-input-copy").click(function () {
-    let val = $(this).parent().children("input").data("value");
-    var $tmp = $("<textarea>");
-    $("body").append($tmp);
-    $tmp.val(val).select();
-    document.execCommand("copy");
-    $tmp.remove();
-  });
-  
 });
-
-
-
-const htmlToRender = `
-    <section class="transaction">
-    <div class="container transaction__container wow animate__fadeInUp">
-        <h1 class="transaction__title">
-            Обмен <br>
-            Ethereum (ETH) on Dogecoin (DOGE)                    </h1>
-        <div class="transaction__order">
-            Номер заявки <span>63910cb54805b</span>
-        </div>
-                                                                                            <div class="transaction-4">
-                <div class="transaction__status transaction__status_blue">
-                    ЖДЕМ ПОДТВЕРЖДЕНИЙ
-                </div>
-                <div class="transaction__status-text">
-                    Ждем хотя бы одно подтверждение!
-                </div>
-            </div>
-                            <div class="transaction__btns">
-            <a class="transaction__btn" href="./index.html">
-                <img src="./assets/img/transaction/home.svg" alt="home">
-                Вернуться
-            </a>
-                                </div>
-    </div>
-    </section>
-`
